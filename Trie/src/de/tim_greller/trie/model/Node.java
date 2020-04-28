eef2bf54e52df43cb4b67c9f3946fd4ff890de4d
Tim Greller <timlg07@gmail.com>
2020-04-28 15:20:33 +0200

    created constructors

--- Trie/src/de/tim_greller/trie/model/Node.java
+++ Trie/src/de/tim_greller/trie/model/Node.java
@@ -2,4 +2,14 @@ package de.tim_greller.trie.model;
 
 public class Node {
 
+	char character;
+	Node parent;
+	Node[] children;
+	
+	public Node(char character, Node parent) {
+		this.character = character;
+		this.parent = parent;
+		this.children = new Node[25];
+	}
+
 }
--- Trie/src/de/tim_greller/trie/model/Trie.java
+++ Trie/src/de/tim_greller/trie/model/Trie.java
@@ -1,5 +1,11 @@
 package de.tim_greller.trie.model;
 
 public class Trie {
+	
+	Node root;
+	
+	public Trie() {
+		root = new Node('+', null);
+	}
 
 }
