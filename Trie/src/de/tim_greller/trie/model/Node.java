770530b188382e585478db1d1d926f29f526302d
Tim Greller <timlg07@gmail.com>
2020-04-29 11:32:53 +0200

    added public methods to Trie

--- Trie/src/de/tim_greller/trie/model/Node.java
+++ Trie/src/de/tim_greller/trie/model/Node.java
@@ -1,12 +1,13 @@
 package de.tim_greller.trie.model;
 
-public class Node {
+public class Node<T> {
 
 	char character;
-	Node parent;
-	Node[] children;
+	Node<T> parent;
+	Node<T>[] children;
+	T value;
 	
-	public Node(char character, Node parent) {
+	public Node(char character, Node<T> parent) {
 		this.character = character;
 		this.parent = parent;
 		this.children = new Node[25];
--- Trie/src/de/tim_greller/trie/model/Trie.java
+++ Trie/src/de/tim_greller/trie/model/Trie.java
@@ -2,10 +2,31 @@ package de.tim_greller.trie.model;
 
 public class Trie<T> {
 	
-	Node root;
+	Node<T> root;
 	
 	public Trie() {
-		root = new Node('+', null);
+		root = new Node<T>('+', null);
+	}
+
+	public boolean add(String key, T val) {
+		return true;
+	}
+	
+	public boolean remove(String key) {
+		return true;
+	}
+	
+	public boolean change(String key, T newValue) {
+		return true;
+	}
+	
+	public T getValue(String key) {
+		return root.value;
+	}
+	
+	@Override
+	public String toString() {
+		return root.toString();
 	}
 
 }
